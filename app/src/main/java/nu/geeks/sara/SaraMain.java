67332326c551fbffa2802354fafb8a2cd5ef36eb
HANNES PAULSSON <hannes.paulsson@gmail.com>
2015-04-10 17:01:01 +0200

    Added bluetooth support! Basic as fudge, but we can send and recieve data. No ide how a lot of it works though. :)

--- app/src/main/java/nu/geeks/sara/SaraMain.java
+++ app/src/main/java/nu/geeks/sara/SaraMain.java
@@ -1,15 +1,30 @@
 package nu.geeks.sara;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TooManyListenersException;
+import java.util.UUID;
 
 
 /*
@@ -27,6 +42,15 @@ The app is locked in portrait mode. This is done in AndroidManifest.xml with the
 public class SaraMain extends Activity implements SensorEventListener {
 
 
+    private static String address;
+
+    Handler bluetoothIn;
+
+    final int handlerState = 0;                        //used to identify handler message
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private StringBuilder recDataString = new StringBuilder();
+    private ConnectedThread mConnectedThread;
 
 
     //double acc[] = new double[3];
@@ -34,10 +58,22 @@ public class SaraMain extends Activity implements SensorEventListener {
     private SensorManager sensorManager;
     TextView text1, text2;
 
+    Boolean bluetoothEnable = true;
+
+
+    private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+
+    private final int REQUEST_ENABLE_BT = 1234;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sara_main);
+
+
+          bluetoothConnection();
 
         //Initializing sensor, creating a sensor manager.
         sensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
@@ -74,6 +110,7 @@ public class SaraMain extends Activity implements SensorEventListener {
                 Simply change the textvalue to the current progress.
                  */
                 text1.setText("Speed: " + (progress - 50)/5);
+                mConnectedThread.write(Integer.toString(progress));
             }
 
             @Override
@@ -89,6 +126,82 @@ public class SaraMain extends Activity implements SensorEventListener {
 
     }
 
+    private void bluetoothConnection(){
+
+        //Initialize bluetooth
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(btAdapter == null){
+            //If bluetoothadapter == null, the device does not support bluetooth.
+            bluetoothEnable = false;
+            Toast.makeText(getApplicationContext(), "Bluetooth not supported!", Toast.LENGTH_LONG).show();
+
+        }else{
+            Toast.makeText(getApplicationContext(), "Bluetooth is supported!", Toast.LENGTH_LONG).show();
+        }
+
+        if (!btAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+
+        // If there are paired devices
+        if (pairedDevices.size() > 0) {
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                // Add the name and address to an array adapter to show in a ListView
+                if(device.getName().equals("HC-06")){
+                    address = device.getAddress();
+                    Toast.makeText(getApplicationContext(), "CONNECTED TO SARA!", Toast.LENGTH_LONG).show();
+                }
+                }
+            }
+        }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        //Get MAC address from DeviceListActivity via intent
+        Intent intent = getIntent();
+
+        //Get the MAC address from the DeviceListActivty via EXTRA
+        //create device and set the MAC address
+        BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+        try {
+            btSocket = createBluetoothSocket(device);
+        } catch (IOException e) {
+            Toast.makeText(getBaseContext(), "Socket creation failed", Toast.LENGTH_LONG).show();
+        }
+        // Establish the Bluetooth socket connection.
+        try
+        {
+            btSocket.connect();
+        } catch (IOException e) {
+            try
+            {
+                btSocket.close();
+            } catch (IOException e2)
+            {
+                //insert code to deal with this
+            }
+        }
+        mConnectedThread = new ConnectedThread(btSocket);
+        mConnectedThread.start();
+
+        //I send a character when resuming.beginning transmission to check device is connected
+        //If it is not an exception will be thrown in the write method and finish() will be called
+        mConnectedThread.write("x");
+    }
+
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+
+        return  device.createRfcommSocketToServiceRecord(BTMODULEUUID);
+        //creates secure outgoing connecetion with BT device using UUID
+    }
 
     /* This has to be defined when implementing SensorEventListener.
     *   This is the method that is called when the sensor values changes (which is pretty much
@@ -123,8 +236,19 @@ public class SaraMain extends Activity implements SensorEventListener {
     //Has to be defined when implementing sensorEventListener
     public void onAccuracyChanged(Sensor sensor, int accuracy){}
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_ENABLE_BT){
+            if(resultCode == RESULT_OK){
+                Toast.makeText(getApplicationContext(), "Bluetooth enabled!", Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(getApplicationContext(), "Bluetooth NOT enabled!", Toast.LENGTH_LONG).show();
+            }
+        }
 
-    /*
+    }
+
+/*
     Stuff below is auto generated code. Don't think we need to care about it.
      */
 
@@ -139,9 +263,6 @@ public class SaraMain extends Activity implements SensorEventListener {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -151,4 +272,56 @@ public class SaraMain extends Activity implements SensorEventListener {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+//create new class for connect thread
+private class ConnectedThread extends Thread {
+    private final InputStream mmInStream;
+    private final OutputStream mmOutStream;
+
+    //creation of the connect thread
+    public ConnectedThread(BluetoothSocket socket) {
+        InputStream tmpIn = null;
+        OutputStream tmpOut = null;
+
+        try {
+            //Create I/O streams for connection
+            tmpIn = socket.getInputStream();
+            tmpOut = socket.getOutputStream();
+        } catch (IOException e) { }
+
+        mmInStream = tmpIn;
+        mmOutStream = tmpOut;
+    }
+
+    public void run() {
+        byte[] buffer = new byte[256];
+        int bytes;
+
+        // Keep looping to listen for received messages
+        while (true) {
+            try {
+                bytes = mmInStream.read(buffer);            //read bytes from input buffer
+                String readMessage = new String(buffer, 0, bytes);
+                // Send the obtained bytes to the UI Activity via handler
+                bluetoothIn.obtainMessage(handlerState, bytes, -1, readMessage).sendToTarget();
+            } catch (IOException e) {
+                break;
+            }
+        }
+    }
+    //write method
+    public void write(String input) {
+        byte[] msgBuffer = input.getBytes();           //converts entered String into bytes
+        try {
+            mmOutStream.write(msgBuffer);                //write bytes over BT connection via outstream
+        } catch (IOException e) {
+            //if you cannot write, close the application
+            Toast.makeText(getBaseContext(), "Connection Failure", Toast.LENGTH_LONG).show();
+            finish();
+
+        }
+    }
 }
+}
+
